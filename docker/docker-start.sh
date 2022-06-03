65d1d9c4d020b0c930aed20069b7ec2f96c146b0
Dmitry Vasilev <dmitry@t3dev.ru>
2022-06-03 14:05:08 +0300

    Add docker files

---
+++ docker/docker-start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker start test_project_web
+docker start test_project_phpmyadmin
+docker start test_project_mysql
+docker start test_project_mailhog
---
+++ docker/docker-stop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker stop test_project_web
+docker stop test_project_phpmyadmin
+docker stop test_project_mysql
+docker stop test_project_mailhog
